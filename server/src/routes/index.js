18cad69a7a7a0da99349628a97bc75a97e889fbd
Jt2572 <95650277+Jt2572@users.noreply.github.com>
2023-01-03 12:43:32 -0500

    routes

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,7 +1,9 @@
 const { Router } = require("express");
 
+const product = require('./product.js')
+
 const router = Router();
 
 // Configurar los routers
-
+router.use('/products', product)
 module.exports = router;
