7aff0318e1d0f13df44e66467ad415e79913fdc7
Jt2572 <95650277+Jt2572@users.noreply.github.com>
2023-02-08 13:27:43 -0500

    Update Landing.jsx

--- client/src/components/Landing.jsx
+++ client/src/components/Landing.jsx
@@ -7,19 +7,12 @@ const LandingPage = () => {
     
     return (
         
-        <div >
+        <>
           <Header/>
             
-            <main >
-                {/* Aquí va el contenido principal de la landing page */}
-            </main>
-            <footer >
-                {/* Aquí van los elementos del pie de página */}
-            </footer>
-        </div>
+        </>
     );
 };
 
 export default LandingPage;
 
-        //   Esta estructura utiliza la sintaxis de componentes funcionales de React y define un componente LandingPage que se encarga de renderizar todos los elementos de la landing page. La estructura HTML se organiza en tres secciones principales: header, main y footer, cada una con su propio contenido y estilos. Esto es solo un ejemplo básico, pero puedes agregar o personalizar los componentes según tus necesidades.
