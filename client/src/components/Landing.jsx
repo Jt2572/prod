e20b619ae05e75e75f1383fb039de54f297626b4
Jt2572 <95650277+Jt2572@users.noreply.github.com>
2023-02-08 13:34:06 -0500

    Update Landing.jsx

--- client/src/components/Landing.jsx
+++ client/src/components/Landing.jsx
@@ -1,15 +1,31 @@
-import Header from "./Header";
+// import Header from "./Header";
+import Navigation from "./Navigation";
+import image from '../assets/LandingPageBG.png'
 
 
 
 
 const LandingPage = () => {
-    
+
     return (
-        
+
         <>
-          <Header/>
-            
+            {/* <Header/> */}
+            <header  >
+
+                <Navigation />
+            </header>
+
+            <section className="header__container">
+                <img src={image} className="landing__image" alt="LandingPage" />
+                <div className="landing">
+                    <h1 className="landing__title">Incrementa tus ventas con productos publicitarios de alta calidad</h1>
+                    <p className="landing__subtitle">Tarjetas, Membretes, Talonarios, Flyers, Brochures, Pendones y
+                        una amplia gama de productos tanto litograficos como digitales</p>
+                    <button>Comprar Ahora</button>
+                </div>
+            </section>
+
         </>
     );
 };
