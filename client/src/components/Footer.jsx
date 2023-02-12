4a02b99c387de49addf45a450e28705e09039896
Jt2572 <95650277+Jt2572@users.noreply.github.com>
2023-02-12 18:55:40 -0500

    update styles

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,39 @@
+import { FaFacebook, FaInstagram } from 'react-icons/fa';
+import logoFooter from "../assets/logoPubligrafWhite.svg"
+const Footer = () => {
+    return (
+
+        <footer >
+            <div className="footer__container">
+                <div className="footer__container__50pc">
+                    <img src={logoFooter} width={80} alt="publigraf" />
+
+                    <p>Proporcionamos productos publicitarios litográficos y digitales económicos y de alta calidad</p>
+                </div>
+
+                <div className='footer__contactContainer'>
+
+                    <div className="footer__container__25pc">
+                        <h3 >Contactos</h3>
+                        <ul>
+                            <li>Email: info@example.com</li>
+                            <li>Cel: 000 000 0000</li>
+                        </ul>
+                    </div>
+                    <div className="footer__container__25pc">
+                        <h3>Nuestras redes</h3>
+                        <ul className="footer__container__25pc__socialIcons">
+                            <li><FaFacebook /> cuentafb</li>
+                            <li><FaInstagram /> cuentaInstagram</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            {/* <div className="footer__copyright">
+                
+                <p>Copyright &copy; 2023 Example Co. All rights reserved.</p>
+            </div> */}
+        </footer>
+    )
+}
+export default Footer;
